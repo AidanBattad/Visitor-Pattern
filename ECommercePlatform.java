243b3ad07313b8ccb02f8c2e95cb042797fa4cd2
Aidan Liam N. Battad <142377920+AidanBattad@users.noreply.github.com>
2024-05-19 22:23:19 +0800

    ECommercePlatform.java

--- ECommercePlatform.java
+++ ECommercePlatform.java
@@ -1,35 +1,29 @@
-package visitorPattern;
 
 public class ECommercePlatform {
-
     public static void main(String[] args) {
-        
-        Shipping chair = new Furniture("Chair", "Lightweight", 400, 5);
-        Shipping sofa = new Furniture("Sofa", "Heavyweight", 3000, 4);
-        Shipping bed = new Furniture("Bed", "Heavyweight", 7000, 9);
-        Shipping desk = new Furniture("Desk", "Lightweight", 600, 2);
+        Shipping chair = new Chair(8);  
+        Shipping sofa = new Sofa(6);    
+        Shipping bed = new Bed(12);     
+        Shipping desk = new Desk(3);    
 
-        ShippingDetails details = new FurnitureInformation();
-        ShippingCostCalculator calculator = new ShippingCost();
+        ShipInfo info = new FurnitureInfo();
+        ShippingCostCalcu calculator = new ShippingCost();
 
         System.out.println("FURNITURE SHOP\n");
-
         System.out.println("Order Details\n");
 
-        System.out.println(details.showFurnitureInformation(chair.getFurnitureType(), chair.getFurnitureSize()));
-        System.out.println("Distance: " + chair.getDistance() + "km");
-        System.out.println("Shipping Cost: ₱" + calculator.calculateShippingCost(chair.getFurnitureType(), chair.getDistance()) + "\n");
-        
-        System.out.println(details.showFurnitureInformation(sofa.getFurnitureType(), sofa.getFurnitureSize()));
-        System.out.println("Distance: " + sofa.getDistance() + "km");
-        System.out.println("Shipping Cost: ₱" + calculator.calculateShippingCost(sofa.getFurnitureType(), sofa.getDistance()) + "\n");
-
-        System.out.println(details.showFurnitureInformation(bed.getFurnitureType(), bed.getFurnitureSize()));
-        System.out.println("Distance: " + bed.getDistance() + "km");
-        System.out.println("Shipping Cost: ₱" + calculator.calculateShippingCost(bed.getFurnitureType(), bed.getDistance()) + "\n");
-        
-        System.out.println(details.showFurnitureInformation(desk.getFurnitureType(), desk.getFurnitureSize()));
-        System.out.println("Distance: " + desk.getDistance() + "km");
-        System.out.println("Shipping Cost: ₱" + calculator.calculateShippingCost(desk.getFurnitureType(), desk.getDistance()) + "\n");
-
-    }}
+        displayOrderDetails(chair, info, calculator);
+        displayOrderDetails(sofa, info, calculator);
+        displayOrderDetails(bed, info, calculator);
+        displayOrderDetails(desk, info, calculator);
+    }
+    private static void displayOrderDetails(Shipping furniture, ShipInfo info, ShippingCostCalcu calculator) {
+        System.out.println(furniture.accept(info));
+        System.out.println("Distance: " + furniture.getDistance() + "km");
+        double shippingCost = furniture.accept(calculator);
+        double totalPrice = shippingCost + furniture.getBasePrice();
+        System.out.println("Base Price: ₱" + furniture.getBasePrice());
+        System.out.println("Shipping Cost: ₱" + shippingCost);
+        System.out.println("Total Cost: ₱" + totalPrice + "\n");
+    }
+}
